ba3fa61f196cbd3e13a4642b01fb3e8af8279e18
zhangli <313890735@qq.com>
2016-02-21 17:33:09 +0800

    粗糙的二维码

---
+++ QRCode/QRCode/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  QRCode
+//
+//  Created by 余亮 on 16/2/21.
+//  Copyright © 2016年 余亮. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <CoreImage/CoreImage.h>
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageV;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    //1.常滤镜对象
+    CIFilter * filter = [CIFilter filterWithName:@"CIQRCodeGenerator"]; //CIQRCodeGenerator：固定写法
+    
+    //2.设置二维码的数据（通过KVC）
+        //需要把字符串转为NSDate
+//    NSString * NameStr = @"手扶拖拉机";   //扫描一个字符串
+    NSString * UrlStr = @"https://github.com/YL19930403";   //扫描一个连接
+    NSData * data = [UrlStr dataUsingEncoding:NSUTF8StringEncoding];
+    [filter setValue:data forKeyPath:@"inputMessage"];
+    
+    //3.获取二维码
+    CIImage * outputImage = [filter outputImage];
+    self.imageV.image = [UIImage imageWithCIImage:outputImage];
+}
+
+@end
